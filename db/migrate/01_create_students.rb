12f09e7d507257a200c2fcef2b8e9098f239e148
Amanda Ackerman <ackerm44@gmail.com>
2017-11-30 18:30:31 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,11 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
-  def create_tablee
+  def change
     create_table :artists do |t|
       t.string :name
     end
-  end
-
-  def change
   end
 
 end
