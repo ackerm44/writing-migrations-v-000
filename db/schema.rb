b80f1c9d2db62eed3ccb23c7b40f00e3caa4a157
Amanda Ackerman <ackerm44@gmail.com>
2017-11-30 18:35:27 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
