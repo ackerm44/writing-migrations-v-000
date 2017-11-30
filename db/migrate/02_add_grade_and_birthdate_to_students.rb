6a7fdf115953d57a369ce821df28f38aef29c4b2
Amanda Ackerman <ackerm44@gmail.com>
2017-11-30 18:40:40 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
-    :artists, :favorite_food, :string
     add_column :students :grade :integer
     add_column :students :birthdate :string
   end
